94942f8692b035b00ab99866d5b49ef70b10ee94
gopikap <gopika.krish@gmail.com>
2014-09-02 18:45:54 +0530

    Working with oneNote

--- blocks/onenote/block_onenote.php
+++ blocks/onenote/block_onenote.php
@@ -1,8 +1,5 @@
 <?php
 require_once($CFG->dirroot.'/blocks/onenote/onenote_lib.php');
-//require_once($CFG->dirroot.'/blocks/onenote/onenote.html');
-
-
 class block_onenote extends block_list {
     public function init() {
         $this->title = get_string('onenote', 'block_onenote');
@@ -35,16 +32,13 @@ class block_onenote extends block_list {
         if (!empty($code)) {
             $authprovider = required_param('authprovider', PARAM_ALPHANUMEXT);
         }
-        echo "CODE".$code;
-        echo "<br>";
-        //echo "AACODE".$authprovider;
       
         // OneNote client application settings
         $client_id = '00000000401290EF'; //'Mc8gpyG9wYr7f5qSr8JoQ';
-        $client_secret = 'Qpa5MN9UK215hqRcefdtVMaSGwHoX2H5'; //'hvzBKUtj3cFkmIxjioqvSpAnNkfTfTT5X7lP8lgIOP0';
+        $client_secret = 'cKyZUs3chL6Z9SXTG5y9Ub2vL-5SctWm'; //'hvzBKUtj3cFkmIxjioqvSpAnNkfTfTT5X7lP8lgIOP0';
         $redirect_uri = 'http://gopikalocal.com/blocks/onenote/onenote_redirect.php';//http://vinlocaldomain.com:88 // 'http://localhost/moodleapp/blocks/yammer/yammer_redirect.php';
         $scopes = 'wl.signin';
-        $response_type = 'token';
+        $response_type = 'code';
 
         $curl = new curl();
         if($code && $authprovider == 'onenote') {            
@@ -52,35 +46,39 @@ class block_onenote extends block_list {
             $response = $curl->get('https://login.live.com/oauth20_token.srf?client_id='.$client_id.'&client_secret='.$client_secret.'&code='.$code.'&redirect_uri='.$redirect_uri.'&grant_type=authorization_code');
             
             error_log(print_r($response, true));
-           // $response = json_decode($response);
-            //$SESSION->onenotetoken = $response->access_token->token;
+            $response = json_decode($response);            
+            $SESSION->onenotetoken = $response->access_token;
         }  
         if(!isset($SESSION->onenotetoken) ) {
             $content->items[] = "<a class='zocial' href='https://login.live.com/oauth20_authorize.srf?client_id=$client_id&redirect_uri=$redirect_uri&scope=$scopes&response_type=$response_type'>Sign in with OneNote</a>";
         }
         else {
-            // $messages = get_yammer_private_messages($SESSION->yammertoken);
-            // $messages = json_decode($messages);
-            // $reference_array =$messages->references;
-
-            // $content->items[] = "<table border='0' cellpadding='4' cellspacing='4'><tr><td style='width:70px;font-weight:bold;'>From</td><td style='font-weight:bold;'>Message</td><td style='width:70px;font-weight:bold;'>Date</td></tr>";
-            // foreach($messages->messages as $message) {
-                // foreach($reference_array as $sender) {
-                    // if($message->sender_id == $sender->id) {
-                        // $name = $sender->full_name;
-                    // }
-                // }
-
-                // $created_at = strtotime($message->created_at);
-                // $created_at = date("M j G:i:s",$created_at);
-
-                // $content->items[] ="<tr><td style='vertical-align:top;width:70px;'>".$name.
-                    // "</td><td style='vertical-align:top;'>".$message->body->plain."</td><td style='vertical-align:top;width:70px;'>".$created_at."</td></tr>";
-            // }
-
-            // $content->items[] = "</table>";
+            
+            $param = array(
+                      "name" => "testnote"   
+                     );
+            $encoded_param = json_encode($param);
+            $curl = new curl();
+            $header = array(
+                        'Authorization: Bearer ' . $SESSION->onenotetoken,
+                        'Content-Type: application/json'
+            );
+            $curl->setHeader($header);
+            $eventresponse = $curl->post('https://www.onenote.com/api/v1.0/notebooks',$encoded_param); // TODO: Restrict time range to be the same as moodle events
+        
+            print_r($eventresponse);
+            //Create a link and call the ajax on submit. Uncomemtn the onenote.html to get the ajax call.                     
+            //$content->items[] = "<span>Create notebook</span>";
+            /*$content->items[] = "<div id='create'>                                 
+                                    <input id='token' type='hidden' name='token' value=".$SESSION->onenotetoken.">
+                                    <input id='newnote' type='text' name='newnote' />
+                                    <input type='submit' id='save' name='submit' value='Save'>                                
+                                </div>";*/
+           error_log(print_r($eventresponse, true));
+           $content->items[] = "Create notbook";
         }
 
         return $content;
     }
 }
+//require_once($CFG->dirroot.'/blocks/onenote/onenote.html');
--- blocks/onenote/onenote_redirect.php
+++ blocks/onenote/onenote_redirect.php
@@ -6,13 +6,13 @@
  */
 require('../../config.php');
 //echo $_GET['access_token'];exit;
-$code = optional_param('access_token', '', PARAM_TEXT);
+$code = optional_param('code', '', PARAM_TEXT);
 
 if (empty($code)) {
     throw new moodle_exception('onenote_failure');
 }
 
-$loginurl = '/my/'; // TODO: What should be this url to allow user to add the block on other pages?
+$loginurl = '/'; // TODO: What should be this url to allow user to add the block on other pages?
 if (!empty($CFG->alternateloginurl)) {
     $loginurl = $CFG->alternateloginurl;
 }
