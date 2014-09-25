cf1e3a7e96b9c9416d6e2bdda964534895cf35bf
vinhub <vin.bhalerao@gmail.com>
2014-09-25 09:40:26 -0700

    started adding onenote file submission plugin code

---
+++ plugins/mod/assign/submission/onenote/version.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the version information for the OneNote submission plugin
+ *
+ * @package    assignsubmission_onenote
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2014092400;
+$plugin->requires  = 2014050800;
+$plugin->component = 'assignsubmission_onenote';
