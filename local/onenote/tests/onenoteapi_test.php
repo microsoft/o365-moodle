d4da646848d30057069a34f4d5cb03e405243a1a
vinhub <vin.bhalerao@gmail.com>
2014-11-24 17:49:58 -0800

    Added phpunit tests from OnenoteTest_master branch

--- local/onenote/tests/onenoteapi_test.php
+++ local/onenote/tests/onenoteapi_test.php
@@ -25,9 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 global $CFG;
-require_once($CFG->dirroot.'/lib/oauthlib.php');
-require_once($CFG->dirroot.'/repository/onenote/onenote_api.php');
+require_once($CFG->dirroot.'/local/onenote/onenote_api.php');
 require_once($CFG->dirroot . '/mod/assign/tests/base_test.php');
+
+
 /**
  * Class microsoft_onenote_testcase
  *
@@ -40,85 +41,171 @@ class microsoft_onenote_testcase extends advanced_testcase {
     private $accesstoken;
     /** @var stdClass $user A user to submit an assignment. */
     protected $user;
-
+    protected $user1;
+    
     /** @var stdClass $course New course created to hold the assignment activity. */
     protected $course1;
     protected $course2;
 
     /** @var stdClass $cm A context module object. */
     protected $cm;
+    protected $cm1;
 
     /** @var stdClass $context Context of the assignment activity. */
     protected $context;
+    protected $context1;
 
     /** @var stdClass $assign The assignment object. */
     protected $assign;
-    
+    protected $assign1;
     
     
     public function  setup() {
     	global $DB;    	
     	$this->resetAfterTest(true);
     	$this->user = $this->getDataGenerator()->create_user();
+    	$this->user1 = $this->getDataGenerator()->create_user();
     	$this->course1  = $this->getDataGenerator()->create_course();
-    	$this->course2 = $this->getDataGenerator()->create_course();
-    	
+    	$this->course2 = $this->getDataGenerator()->create_course();    	
     	//setting user and enrolling to the courses created with teacher role.
-    	$this->setUser($this->user->id);
+    	$this->setUser($this->user->id);    	
     	$c1ctx = context_course::instance($this->course1->id);
     	$c2ctx = context_course::instance($this->course2->id);
     	$this->getDataGenerator()->enrol_user($this->user->id, $this->course1->id,4);
     	$this->getDataGenerator()->enrol_user($this->user->id, $this->course2->id,4);
-    	$this->assertCount(1, get_enrolled_users($c1ctx));
-    	$this->assertCount(1, get_enrolled_users($c2ctx));
     	$this->assertCount(2, enrol_get_my_courses());
     	$courses = enrol_get_my_courses();
+    	//student enrollment
+    	$this->setUser($this->user1->id);    	
+    	$this->getDataGenerator()->enrol_user($this->user1->id, $this->course1->id,5);
+    	$this->getDataGenerator()->enrol_user($this->user1->id, $this->course2->id,5);
     	 
-    	$this->onenote_api = microsoft_onenote::get_onenote_api();
-    //	error_log("incourse");
-    //	error_log(print_r($courses,true));
-   // 	error_log("inenrolcourse");
-    	/*$token = new stdClass();
-    	$token->token = "EwB4Aq1DBAAUGCCXc8wU/zFu9QnLdZXy+YnElFkAAbJ035V/2j8ddS2CiJoMRf938a1DxEtErYcOSNmU8FF8lN10sdICPX2s6o42PTR3G1TE0cVGLGGUs1/nzRWjlx3HRKYW7bGBUPCba8XExfLsjs2FVLZBMG0gUHAA1e54xqp3qI1nYHzN9P16u9O6oHuDTP6njK6GCoamsVL1naxoSCCWMRs/2t1CHiSUB1WQ2zMo5g5dRzvxp+4SCISIo5UcSBuzzsKVX/pPR1JoxZTQLbFrogQYxc2YrPtoUX8riw0OiosOVgSUyQDKRDtQEZwdNGKR2NijNc6EDf35ljo3b29FVYT7RQahG6M5I7h50eXYK4CvUoQO6ZRAwwHnqSADZgAACKfRC9CAJ+LoSAFtg/G8N7RlRlQvkgjyULJaDa34gWN+Wex/CrUF16SF/hgOB56G8kziTs5ub8wK4cI5yE57vNQ3Xc7BfHFkBKLH2UR0lSc9pm+wu7WKBEC5T2th4JcqrqpK1vqWcqOxQFTwj6UM6mPu+zsQJ2QjE4NYyd6HdpqHdk8iNbc3phvdW/oXflH/Tl8QJY7Gu7WWjy/nk//h0ZG5G3//kbFWzF+zO+tdtMbPxGjmxw5FNRsxKg956pt8IEsl9UhZvWa/tSh219gFgqn+xyLpejmaho6Of0zkaPSu2NLMq6hzJM+NI6dHM+eL3Y3OhcDg9616wGFwj2unwCl8PD893TrCIDHN1PUrxgDOLFr1WJC06ZavYMtApHB8XkaiGxhJJR0/zItmtILK7GnJDKLlSsWBsnJ+m9KHZZILrSKPgVQFvye44/93NUDwAOfPZAE=";
-    	$token->expires = "1413460546";
-        $this->setAccessToken($token);
-    	 */
-      	//$onenote_token = $this->onenote_api->get_onenote_token
     	
-    }
+    	$this->assertCount(2, get_enrolled_users($c1ctx));
+    	//$this->assertCount(2, get_enrolled_users($c2ctx));  	
+    	 
+    	$this->onenote_api = onenote_api::getInstance();
+    	
     
-   /* public function setAccessToken($token) {
-    	$this->accesstoken = $token;  
-    }*/
-   
-    public function test_getmicrosoft_repo() {
-       $onenote_id = $this->onenote_api->get_onenote_repo_id();
-       $this->assertNotEmpty($onenote_id,"No value");       	
     }
-    
-   
-    public function test_getitemlist() { 
-    	   	 	
-    	$item_list = $this->onenote_api->get_items_list();    
+
+    public function test_getmicrosoft_repo() {    	
+       	$onenote_id = $this->onenote_api->get_onenote_repo_id();
+       	$this->assertNotEmpty($onenote_id,"No value");       	
+    }
+  
+  
+    public function test_getitemlist() {
+
+    	$this->setUser($this->user->id);
+    	$item_list = $this->onenote_api->get_items_list();
+    	$note_section_names = array();
+    	$course1 = $this->course1->fullname;
+    	$course2 = $this->course2->fullname;
+    	$expected_names = array('Moodle Notebook',$course1,$course2);
+        foreach($item_list as $item)
+    	  if($item['title'] == "Moodle Notebook") {         
+    	  	array_push($note_section_names, "Moodle Notebook");
+    	  	$item_list = $this->onenote_api->get_items_list($item['path']);         	
+            foreach ($item_list as $item) {            	
+            	array_push($note_section_names, $item['title']);            	
+              }       
+    	}         
+       	$this->assertTrue(in_array("Moodle Notebook", $note_section_names),"Moodle Notebook not present");
+       	$this->assertTrue(in_array($course1, $note_section_names),"Test course1 is not present");
+       	$this->assertTrue(in_array($course2, $note_section_names),"Test course2 is  not present");         
+        $this->assertTrue(count($expected_names) == count(array_intersect($expected_names, $note_section_names)), "Same elements are not present");
     	$this->assertNotEmpty($item_list,"No value");
     }
     
-    
+  
      public function test_getpage() {
-     	
-     	$item_list = $this->onenote_api->get_items_list();
+     	//$this->settoken();
+     	$this->setUser($this->user->id);
+     	$item_list = $this->onenote_api->get_items_list();     	
+        
+        //Creating a testable assignment
      	$generator = $this->getDataGenerator()->get_plugin_generator('mod_assign');
      	$params['course'] = $this->course1->id;
      	$instance = $generator->create_instance($params);
      	$this->cm = get_coursemodule_from_instance('assign', $instance->id);
      	$this->context = context_module::instance($this->cm->id);
-     	$this->assign = new testable_assign($this->context, $this->cm, $this->course1);
-     	$assign_details = $this->assign->get_instance();
-     	$assign_id = $assign_details->id; 
-     	$save_assigment = $this->onenote_api->get_page($assign_id,false);
-      	print_r($save_assigment);
+     	$this->assign = new testable_assign($this->context, $this->cm, $this->course1);        
+        $assign_details = $this->assign->get_instance();
+        $assign_id = $assign_details->id;
+        
+     	//To get the notebooks of student 
+     	$this->setUser($this->user1->id);
+     	$item_list = $this->onenote_api->get_items_list();     
+     	
+     	//Student submission to onenote
+     	
+     	$create_submission = $this->create_submission_feedback($this->cm,false,false,null,null,null);      	     	           	
+     	$this->submission = $this->assign->get_user_submission($this->user1->id, true);     	
+     	//Saving the assignment
+     	$data = new stdClass();
+     	$saveassign = new assign_submission_onenote($this->assign, '');
+     	$save_assign = $saveassign->save($this->submission, $data);
+     	
+     	//Creating feedback for submission
+     	$this->setUser($this->user->id);     	
+     	//Saving the grade
+     	$this->grade = $this->assign->get_user_grade($this->user1->id, true);
+     	$gradeassign = new assign_feedback_onenote($this->assign, '');
+     	$grade_assign = $gradeassign->save($this->grade, $data);
+     	$grade_id = $this->grade->grade;          	
+     	$create_feedback = $this->create_submission_feedback($this->cm,true,true,$this->user1->id,$this->submission->id,$grade_id);
+     	   
+     	
+     	if(filter_var($create_submission, FILTER_VALIDATE_URL)) {
+     		if(strpos($this->course1->fullname, urldecode($create_submission))) {
+				$this->assertTrue("The value is present");     				
+     		}
+     	} 
+
+     	if(filter_var($create_feedback, FILTER_VALIDATE_URL)) {
+     		if(strpos($this->course1->fullname, urldecode($create_feedback))) {
+     			$this->assertTrue("The value is present");
+     		}     	
+     	}
      	
      }
+     
+     public function test_downloadpage() {
+        global $DB;
+     	
+        $this->setUser($this->user->id);
+     	$item_list = $this->onenote_api->get_items_list();     	     	
+     	$generator = $this->getDataGenerator()->get_plugin_generator('mod_assign');
+     	$params['course'] = $this->course2->id;
+     	$instance = $generator->create_instance($params);
+     	$this->cm = get_coursemodule_from_instance('assign', $instance->id);
+     	$this->context = context_module::instance($this->cm->id);
+     	$this->assign = new testable_assign($this->context, $this->cm, $this->course2);
+     	$assign_details = $this->assign->get_instance();
+     	$assign_id = $assign_details->id;
+     	
+     	//To get the notebooks of student
+     	$this->setUser($this->user1->id);
+     	$item_list = $this->onenote_api->get_items_list();
+     	
+		$create_submission = $this->create_submission_feedback($this->cm,false,false,null,null,null);      	     	           	
+     	$this->submission = $this->assign->get_user_submission($this->user1->id, true);     	
+     	//Saving the assignment
+     	$data = new stdClass();
+     	$saveassign = new assign_submission_onenote($this->assign, '');
+     	$save_assign = $saveassign->save($this->submission, $data);     	
+     	
+     	$this->assertNotEmpty($save_assign,"File has not created");
 
+     }
+     
+     public function create_submission_feedback($cm,$want_feedback_page = false, $is_teacher = false, $submission_user_id = null, $submission_id = null, $grade_id = null) {
+     	$submission_feedback = $this->onenote_api->get_page($cm->id, $want_feedback_page, $is_teacher, $submission_user_id, $submission_id, $grade_id);
+     	return $submission_feedback;
+     	
+     }
    
 }
+
+
