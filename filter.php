60bce1cc3d22bc7bc6a0b66f09c03970a6c6f618
sushantgawali <sushant.gawali@gmail.com>
2015-01-07 12:43:00 +0530

    office mix video height changed

--- filter.php
+++ filter.php
@@ -148,6 +148,8 @@ function filter_oembed_officemixcallback($link) {
     // Increase the height and width of iframe.
     $json['html'] = str_replace('width="348"', 'width="480"', $json['html']);
     $json['html'] = str_replace('height="245"', 'height="320"', $json['html']);
+    $json['html'] = str_replace('height="310"', 'height="410"', $json['html']);
+    $json['html'] = str_replace('height="267"', 'height="350"', $json['html']);
     return $json['html'];
 }
 
