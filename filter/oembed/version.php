42fae328bc495096a930c1383af28e230317e6b6
James McQuillan <james.mcquillan@remote-learner.net>
2015-02-05 09:51:28 -0500

    Updated oembed filter version file

--- filter/oembed/version.php
+++ filter/oembed/version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014062400;
+$plugin->version   = 2015012700;
 $plugin->requires  = 2014051200;
 $plugin->component = 'filter_oembed';
 $plugin->maturity = MATURITY_STABLE;
