2d051b5b03539a70501f468cc8f0de7415bdc075
James McQuillan <james.mcquillan@remote-learner.net>
2015-02-12 01:09:15 -0500

    MSFTMPP-92: Improvements to setting the connected SharePoint site

--- local/o365/ajax.php
+++ local/o365/ajax.php
@@ -21,13 +21,26 @@
  * @copyright (C) 2014 onwards Microsoft Open Technologies, Inc. (http://msopentech.com/)
  */
 
-defined('MOODLE_INTERNAL') || die();
+define('AJAX_SCRIPT', true);
+require_once(__DIR__.'/../../config.php');
+$mode = required_param('mode', PARAM_TEXT);
 
-$plugin->version = 2015011604;
-$plugin->requires = 2014051200;
-$plugin->component = 'local_o365';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '27.0.0.2';
-$plugin->dependencies = [
-    'auth_oidc' => 2015011600
-];
+require_login();
+require_capability('moodle/site:config', \context_system::instance());
+echo $OUTPUT->header();
+
+$result = new \stdClass;
+$result->success = false;
+
+if ($mode === 'checksharepointsite') {
+    $uncleanurl = required_param('site', PARAM_TEXT);
+    $oidcconfig = get_config('auth_oidc');
+    $httpclient = new \local_o365\httpclient();
+    $clientdata = new \local_o365\oauth2\clientdata($oidcconfig->clientid, $oidcconfig->clientsecret,
+            $oidcconfig->authendpoint, $oidcconfig->tokenendpoint);
+    $result->response = \local_o365\rest\sharepoint::validate_site($uncleanurl, $clientdata, $httpclient);
+    $result->success = true;
+}
+
+echo json_encode($result);
+die();
