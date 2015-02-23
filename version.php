2a0ab3bf1a69e8f20fbfb16b7f7a79c9853d7832
James McQuillan <james.mcquillan@remote-learner.net>
2015-02-23 04:44:34 -0500

    Update version files

--- version.php
+++ version.php
@@ -23,11 +23,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015012702;
+$plugin->version = 2015012703;
 $plugin->requires = 2014111000;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '28.0.0.1';
+$plugin->release = '28.0.0.2';
 $plugin->dependencies = [
     'auth_oidc' => 2015012700
 ];
