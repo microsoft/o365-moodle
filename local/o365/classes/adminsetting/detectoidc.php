6612a4f4a21549e955b4ab45e26b05449e051ba9
James McQuillan <james.mcquillan@remote-learner.net>
2015-03-11 00:13:53 -0400

    MSFTMPP-124, MSFTMPP-135: Improvements to setup page, automate permissions setup, and make setup settings discoverable.

---
+++ local/o365/classes/adminsetting/detectoidc.php
@@ -0,0 +1,99 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package local_o365
+ * @author James McQuillan <james.mcquillan@remote-learner.net>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright (C) 2014 onwards Microsoft Open Technologies, Inc. (http://msopentech.com/)
+ */
+
+namespace local_o365\adminsetting;
+
+/**
+ * Admin setting to detect whether oauth credentials are present in openid connect.
+ */
+class detectoidc extends \admin_setting {
+
+    /**
+     * Constructor.
+     *
+     * @param string $name Name of the setting.
+     * @param string $visiblename Visible name of the setting.
+     * @param string $description Description of the setting.
+     * @param array $defaultsetting Default value.
+     * @param array $choices Array of icon choices.
+     */
+    public function __construct($name, $heading, $description) {
+        $this->nosave = true;
+        parent::__construct($name, $heading, $description, '');
+    }
+
+    /**
+     * Always returns true because we have no real setting.
+     *
+     * @return bool Always returns true
+     */
+    public function get_setting() {
+        return true;
+    }
+
+    /**
+     * Always returns true because we have no real setting.
+     *
+     * @return bool Always returns true
+     */
+    public function get_defaultsetting() {
+        return true;
+    }
+
+    /**
+     * Never write settings.
+     *
+     * @return string Always returns an empty string.
+     */
+    public function write_setting($data) {
+        return '';
+    }
+
+    /**
+     * Return an XHTML string for the setting
+     * @return string Returns an XHTML string
+     */
+    public function output_html($data, $query='') {
+        global $CFG, $OUTPUT;
+
+        $settingspage = new \moodle_url('/admin/settings.php?section=authsettingoidc');
+        $oidcconfig = get_config('auth_oidc');
+        $clientcredspresent = (!empty($oidcconfig->clientid) && !empty($oidcconfig->clientsecret)) ? true : false;
+        $endpointspresent = (!empty($oidcconfig->authendpoint) && !empty($oidcconfig->tokenendpoint)) ? true : false;
+
+        if ($clientcredspresent === true && $endpointspresent === true) {
+            $icon = $OUTPUT->pix_icon('t/check', 'success', 'moodle');
+            $message = \html_writer::tag('span', get_string('settings_detectoidc_credsvalid', 'local_o365'));
+            $linkstr = get_string('settings_detectoidc_credsvalid_link', 'local_o365');
+            $link = \html_writer::link($settingspage, $linkstr, ['style' => 'margin-left: 1rem']);
+            $html = \html_writer::tag('div', $icon.$message.$link, ['class' => 'alert-success local_o365_statusmessage']);
+        } else {
+            $icon = $OUTPUT->pix_icon('t/delete', 'success', 'moodle');
+            $message = \html_writer::tag('span', get_string('settings_detectoidc_credsinvalid', 'local_o365'));
+            $linkstr = get_string('settings_detectoidc_credsinvalid_link', 'local_o365');
+            $link = \html_writer::link($settingspage, $linkstr, ['style' => 'margin-left: 1rem']);
+            $html = \html_writer::tag('div', $icon.$message.$link, ['class' => 'alert-error local_o365_statusmessage']);
+        }
+        return format_admin_setting($this, $this->visiblename, $html, $this->description, true, '', null, $query);
+    }
+}
