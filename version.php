42b82868b9bed8cd835ba0641288a7028708574c
James McQuillan <james.mcquillan@remote-learner.net>
2015-03-15 12:10:02 -0400

    MSFTMPP-136: Fix upgrade step error when trying to add auth_oidc_state/additionaldata

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015011608;
+$plugin->version = 2015011609;
 $plugin->requires = 2014051200;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
