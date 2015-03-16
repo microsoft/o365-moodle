003fbfcdb468622c250e7bb39085c4af9bcbc950
James McQuillan <james.mcquillan@remote-learner.net>
2015-03-16 12:05:38 -0400

    Update version files

--- version.php
+++ version.php
@@ -27,4 +27,4 @@ $plugin->version = 2015012705;
 $plugin->requires = 2014111000;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '28.0.0.2';
+$plugin->release = '28.0.0.3';
