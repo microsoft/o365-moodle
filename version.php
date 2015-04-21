70b4b8148099abd37548987be9d4d68dc4b06c4a
James McQuillan <james.mcquillan@remote-learner.net>
2015-04-21 12:36:15 -0400

    Update version files

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ $plugin->version = 2015011614;
 $plugin->requires = 2014051200;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '27.0.0.6';
+$plugin->release = '27.0.0.7';
 $plugin->dependencies = [
-    'auth_oidc' => 2015011608
+    'auth_oidc' => 2015011612
 ];
