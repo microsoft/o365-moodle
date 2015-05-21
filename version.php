9b4f332b1a7bd2a1533032e7e4d9790c319ae88b
James McQuillan <james.mcquillan@remote-learner.net>
2015-05-21 16:09:41 -0400

    MSFTMPP-181: Expand scope field for auth_oidc_token and local_o365_token to account for expanded permission options

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015012709;
+$plugin->version = 2015012710;
 $plugin->requires = 2014111000;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
