999e8d85d3456c4831ac28008bf9f9f6d4f6eed2
James McQuillan <james.mcquillan@remote-learner.net>
2015-08-13 00:28:09 -0400

    MSFTMPP-209: Ability to automatically match Office 365 users with Moodle users and create a "connected account".

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015012717;
+$plugin->version = 2015012720;
 $plugin->requires = 2014111000;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
