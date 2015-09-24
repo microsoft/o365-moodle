a66864d3fa72d3d6964b8c25245e0c01ad7cbb27
James McQuillan <james.mcquillan@remote-learner.net>
2015-09-24 01:53:05 -0400

    MSFTMPP-165: Migrate local_o365_aaduserdata to local_o365_objects

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015012724;
+$plugin->version = 2015012725;
 $plugin->requires = 2014111000;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
