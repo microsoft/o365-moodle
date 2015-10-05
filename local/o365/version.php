6683c42910a2b7516e3c0108fc047be3bf169316
James McQuillan <james.mcquillan@remote-learner.net>
2015-10-05 13:28:19 -0400

    MSFTMPP-252: Clean up old task records if they were missed by core

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015060110;
+$plugin->version = 2015060111;
 $plugin->requires = 2015051100;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
