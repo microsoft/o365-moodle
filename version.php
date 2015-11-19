bc89d2db6c8ace233877c6115d16607aa1c568b3
James McQuillan <james.mcquillan@remote-learner.net>
2015-11-19 14:28:41 -0500

    MSFTMPP-281: Initial release for Moodle 3.0

--- version.php
+++ version.php
@@ -23,11 +23,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015060115;
-$plugin->requires = 2015051100;
+$plugin->version = 2015111900.00;
+$plugin->requires = 2015111600;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '29.0.0.9';
+$plugin->release = '30.0.0.0';
 $plugin->dependencies = [
-    'auth_oidc' => 2015060107
+    'auth_oidc' => 2015111900.00
 ];
