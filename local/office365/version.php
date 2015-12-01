065cfa644e44ef2462bcfa5c93ba82d538b9219b
James McQuillan <james.mcquillan@remote-learner.net>
2015-12-01 14:47:23 -0500

    MSFTMPP-299: Update version files (m30)

--- local/office365/version.php
+++ local/office365/version.php
@@ -23,19 +23,19 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015111900.00;
+$plugin->version = 2015111901.00;
 $plugin->requires = 2015111600;
 $plugin->component = 'local_office365';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '30.0.0.0';
+$plugin->release = '30.0.0.1';
 $plugin->dependencies = [
-    'auth_oidc' => 2015111900.00,
+    'auth_oidc' => 2015111901.00,
     'block_microsoft' => 2015111900.00,
-    'local_o365' => 2015111900.00,
+    'local_o365' => 2015111901.00,
     'local_onenote' => 2015111900.00,
     'assignfeedback_onenote' => 2015111900.00,
     'assignsubmission_onenote' => 2015111900.00,
     'repository_onenote' => 2015111900.00,
-    'repository_office365' => 2015111900.00,
+    'repository_office365' => 2015111901.00,
     'filter_oembed' => 2015111900.00,
 ];
