6996832050288aca6a34a3940b5ec306da793846
James McQuillan <james.mcquillan@remote-learner.net>
2016-02-01 20:51:56 -0500

    MSFTMPP-339: Update version files (m30)

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015111903;
+$plugin->version = 2015111904;
 $plugin->requires = 2015111600;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '30.0.0.3';
+$plugin->release = '30.0.0.4';
