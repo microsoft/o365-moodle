a4468c3292816365e2cbd3425a933c54517a062c
James McQuillan <james.mcquillan@remote-learner.net>
2016-02-05 01:07:45 -0500

    MSFTMPP-340: Fix logins for Azure users with uppercase characters when using PostgreSQL

--- auth/oidc/version.php
+++ auth/oidc/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015060111;
+$plugin->version = 2015060111.01;
 $plugin->requires = 2015051100;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
