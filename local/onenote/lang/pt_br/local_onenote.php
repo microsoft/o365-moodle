dfae014905e02a1fe93d7babb69c97ce86b77910
James McQuillan <james.mcquillan@remote-learner.net>
2016-02-25 23:55:52 -0500

    MSFTMPP-370: Move notebookname language string to local_onenote

--- local/onenote/lang/pt_br/local_onenote.php
+++ local/onenote/lang/pt_br/local_onenote.php
@@ -5,3 +5,4 @@ $string['feedbacktitle'] = 'Feedback: {$a->assign_name} ({$a->student_firstname}
 $string['connction_error'] = 'Não foi possível conectar-se ao OneNote. Aguarde alguns instantes e tente novamente.';
 $string['onenote_page_error'] = 'Não foi possível abrir a página do OneNote para este envio ou feedback.';
 $string['error_noapiavailable'] = 'Não há nenhuma API do OneNote disponível. Caso você esteja usando o conjunto de plugins do Office 365, não foi possível estabelecer contato com o OneNote. Caso contrário, instale local_msaccount.';
+$string['notebookname'] = 'Bloco de anotações do Moodle';
