5fdd8a0805b65ac8da891567dd8bfc150df242f3
Aashay <aashay@introp.net>
2016-03-18 17:23:49 +0530

    MSFTMPP-341: Updated Power BI oembed filters plug-in code

--- filter/oembed/yui/powerbiloader/powerbiloader.js
+++ filter/oembed/yui/powerbiloader/powerbiloader.js
@@ -1,36 +1,40 @@
-YUI.add('moodle-filter_oembed-powerbiloader', function(Y) {
+YUI.add('moodle-filter_oembed-powerbiloader', function (Y) {
     var ModulenameNAME = 'powerbiloader';
     var token = '';
-    var MODULENAME = function() {
+    var MODULENAME = function () {
         MODULENAME.superclass.constructor.apply(this, arguments);
     };
     Y.extend(MODULENAME, Y.Base, {
-        initializer : function(config) { 
-            token = config.token
-            iframe = document.getElementById('powerbi_iframe');
-            iframe.onload = postActionLoadReport;
+        initializer: function (config) {
+            if (document.getElementsByClassName('token').length > 0)
+            {
+                token = document.getElementsByClassName('token')[0].value;
+                var iframes = document.getElementsByClassName('powerbi_iframe');
+                for (var i = 0; i < iframes.length; i++) {
+                    iframes[i].onload = postActionLoadReport;
+                }
+            }
         }
     }, {
-        NAME : ModulenameNAME, 
-        ATTRS : {
-                 aparam : {}
-        } 
+        NAME: ModulenameNAME,
+        ATTRS: {
+            aparam: {}
+        }
     });
-    M.filter_oembed = M.filter_oembed || {}; 
-                                                 
-    M.filter_oembed.init_powerbiloader = function(config) {
+    M.filter_oembed = M.filter_oembed || {};
+
+    M.filter_oembed.init_powerbiloader = function (config) {
         return new MODULENAME(config);
     };
-    
-    // post the auth token to the iFrame. 
-    postActionLoadReport = function() {
 
-        var m = { action: "loadReport", accessToken: token};
+    // Post the auth token to the iFrame.
+    postActionLoadReport = function () {
+        var m = {action: "loadReport", accessToken: token};
         message = JSON.stringify(m);
-        // push the message.
-        iframe = document.getElementById('powerbi_iframe');
-        iframe.contentWindow.postMessage(message, "*");;
+        // Push the message.
+        this.contentWindow.postMessage(message, "*");
+        ;
     }
-  }, '@VERSION@', {
-      requires:['base']
-  });
+}, '@VERSION@', {
+    requires: ['base']
+});
