e36a2e11bc79f940b92639bc2bf364352777c588
James McQuillan <james.mcquillan@remote-learner.net>
2016-03-22 23:34:30 -0400

    MSFTMPP-378: Clean up and add logging to \local_onenote\api\base

---
+++ local/onenote/classes/utils.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package local_onenote
+ * @author James McQuillan <james.mcquillan@remote-learner.net>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright (C) 2014 onwards Microsoft Open Technologies, Inc. (http://msopentech.com/)
+ */
+
+namespace local_onenote;
+
+/**
+ * General purpose utility class.
+ */
+class utils {
+    public static function tostring($val) {
+        if (is_scalar($val)) {
+            if (is_bool($val)) {
+                return '(bool)'.(string)(int)$val;
+            } else {
+                return '('.gettype($val).')'.(string)$val;
+            }
+        } else if (is_null($val)) {
+            return '(null)';
+        } else {
+            return print_r($val, true);
+        }
+    }
+
+    /**
+     * Record a debug message.
+     *
+     * @param string $message The debug message to log.
+     */
+    public static function debug($message, $where = '', $debugdata = null) {
+        if (class_exists('\local_o365\utils')) {
+            \local_o365\utils::debug($message, $where, $debugdata);
+        }
+    }
+}
