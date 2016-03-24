fd94147742a6065e6e016adef66bc8d7d81aae4a
James McQuillan <james.mcquillan@remote-learner.net>
2016-03-24 13:38:52 -0400

    MSFTMPP-421: Peer review changes

--- auth/oidc/classes/utils.php
+++ auth/oidc/classes/utils.php
@@ -111,6 +111,11 @@ class utils {
         }
     }
 
+    /**
+     * Get the redirect URL that should be set in the identity provider
+     *
+     * @return string The redirect URL.
+     */
     public static function get_redirecturl() {
         global $CFG;
         $wwwroot = (!empty($CFG->loginhttps)) ? str_replace('http://', 'https://', $CFG->wwwroot) : $CFG->wwwroot;
