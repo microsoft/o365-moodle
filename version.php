e1f3164af5e0bf94d2a26e947bf953e6be320d05
James McQuillan <james.mcquillan@remote-learner.net>
2016-05-02 01:06:01 -0400

    MSFTMPP-457: Update version files (v20160425_m30)

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015111906;
+$plugin->version = 2015111907;
 $plugin->requires = 2015111600;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '30.0.0.6';
+$plugin->release = '30.0.0.7';
