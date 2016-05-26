22c1e361a660788529d493a4fc1f2951a62c9579
James McQuillan <james.mcquillan@remote-learner.net>
2016-05-26 16:00:32 -0400

    MSFTMPP-490: Update version files (v20160526_m27)

--- blocks/microsoft/version.php
+++ blocks/microsoft/version.php
@@ -23,12 +23,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015080416;
+$plugin->version = 2015080417;
 $plugin->requires = 2014051200;
 $plugin->component = 'block_microsoft';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '27.0.0.14';
+$plugin->release = '27.0.0.15';
 $plugin->dependencies = [
     'local_onenote' => 2015011614,
-    'local_o365' => 2015011651,
+    'local_o365' => 2015011652,
 ];
