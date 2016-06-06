90cbe5c77f49c963bd38d71c1c2d30fc9dda6fa7
Amy Groshek <amygroshek@gmail.com>
2016-06-06 16:06:48 -0500

    MSFTMPP-474 - Add page to customize Sharepoint subsite creation.

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015012748;
+$plugin->version = 2015012748.01;
 $plugin->requires = 2014111000;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
