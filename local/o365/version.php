60c50531e1feb7ecfce597d1b285b7a69af3aaa5
Aashay <aashay@introp.net>
2016-06-10 19:10:22 +0530

    MSFTMPP-519: Calendar sync feature.

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015011652;
+$plugin->version = 2015011652.01;
 $plugin->requires = 2014051200;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
