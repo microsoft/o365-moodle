f7b19bc3864b83fbc5432561fa9e839b890e17cd
Charles Verge <charles.verge@remote-learner.net>
2016-06-15 22:42:33 -0300

    MSFTMPP-516: Move onenote settings to local plugin

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016062000;
+$plugin->version = 2016062001;
 $plugin->requires = 2016052300;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
