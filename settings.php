6f351354cbdb335fe1f6ad26c11d93058ec03ca8
Charles Verge <charles.verge@remote-learner.net>
2016-06-15 22:42:33 -0300

    MSFTMPP-516: Move onenote settings to local plugin

--- settings.php
+++ settings.php
@@ -136,6 +136,10 @@ if ($hassiteconfig) {
         $desc = new lang_string('settings_sharepointlink_details', 'local_o365');
         $settings->add(new \local_o365\adminsetting\sharepointlink('local_o365/sharepointlink', $label, $desc, '', PARAM_RAW));
 
+        $label = new lang_string('settings_onenote', 'local_o365');
+        $desc = new lang_string('settings_onenote_details', 'local_o365');
+        $settings->add(new \admin_setting_configcheckbox('local_o365/onenote', $label, $desc, '0'));
+
         $label = new lang_string('settings_options_advanced', 'local_o365');
         $desc = new lang_string('settings_options_advanced_desc', 'local_o365');
         $settings->add(new admin_setting_heading('local_o365_options_advanced', $label, $desc));
