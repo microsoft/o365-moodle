e698ad55adae49191fac43bfa63f87facecf289c
Akinsaya Delamarre <adelamarre@remote-learner.net>
2016-06-21 23:29:17 -0400

    MSFTMPP-523: Update parent repository version files (v20160620_m27)

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015011632;
+$plugin->version = 2015011633;
 $plugin->requires = 2014051200;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '27.0.0.24';
+$plugin->release = '27.0.0.25';
