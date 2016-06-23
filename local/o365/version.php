f4290e4c98d3e5aa700b69f8dd6c64ec7ce29555
Nagesh <nagesh@introp.net>
2016-06-23 20:04:20 +0530

    MSFTMPP-149 Class Notebook integration.

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015111917;
+$plugin->version = 2015111917.01;
 $plugin->requires = 2015111600;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
