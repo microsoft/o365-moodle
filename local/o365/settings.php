e80673a18318c1ae03279b15f0e5cf1cc7937ac9
James McQuillan <james@pdyn.net>
2016-07-20 15:06:01 -0400

    MSFTMPP-552: Move Disable OneNote setting to advanced settings

--- local/o365/settings.php
+++ local/o365/settings.php
@@ -140,10 +140,6 @@ if ($hassiteconfig) {
         $desc = new lang_string('acp_sharepointcourseselect_desc', 'local_o365');
         $settings->add(new \local_o365\adminsetting\sharepointcourseselect('local_o365/sharepointcourseselect', $label, $desc, 'off'));
 
-        $label = new lang_string('settings_onenote', 'local_o365');
-        $desc = new lang_string('settings_onenote_details', 'local_o365');
-        $settings->add(new \admin_setting_configcheckbox('local_o365/onenote', $label, $desc, '0'));
-
         $label = new lang_string('settings_options_advanced', 'local_o365');
         $desc = new lang_string('settings_options_advanced_desc', 'local_o365');
         $settings->add(new admin_setting_heading('local_o365_options_advanced', $label, $desc));
@@ -151,6 +147,10 @@ if ($hassiteconfig) {
         $label = new lang_string('settings_o365china', 'local_o365');
         $desc = new lang_string('settings_o365china_details', 'local_o365');
         $settings->add(new \admin_setting_configcheckbox('local_o365/chineseapi', $label, $desc, '0'));
+
+        $label = new lang_string('settings_onenote', 'local_o365');
+        $desc = new lang_string('settings_onenote_details', 'local_o365');
+        $settings->add(new \admin_setting_configcheckbox('local_o365/onenote', $label, $desc, '0'));
 
         $label = new lang_string('settings_enableunifiedapi', 'local_o365');
         $desc = new lang_string('settings_enableunifiedapi_details', 'local_o365');
