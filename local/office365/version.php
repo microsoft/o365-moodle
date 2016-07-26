723e8e1afb1d0651944c9e5c820b112f1d75bd0a
James McQuillan <james.mcquillan@remote-learner.net>
2016-07-25 21:27:57 -0400

    MSFTMPP-556: Update version files (v20160725_m31)

--- local/office365/version.php
+++ local/office365/version.php
@@ -23,15 +23,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016062002;
+$plugin->version = 2016062003;
 $plugin->requires = 2016052300;
 $plugin->component = 'local_office365';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1.0.2';
+$plugin->release = '3.1.0.3';
 $plugin->dependencies = [
     'auth_oidc' => 2016062000,
     'block_microsoft' => 2016062003,
     'local_o365' => 2016062002,
-    'repository_office365' => 2016062002,
+    'repository_office365' => 2016062003,
     'filter_oembed' => 2016062001,
 ];
