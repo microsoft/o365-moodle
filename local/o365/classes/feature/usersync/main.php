2960b9025a9912c1f793ba5369b2ca36fea41581
Rimas Kudelis <rq@akl.lt>
2016-08-20 20:44:53 +0300

    Support fully switching users to Office 365 authentication
    This makes the "Switch matched users to Office 365 authentication" setting behave as advertised.
    Since users are matched even if their username doesn't contain tenant (e.g. "BoB.SmiTh" in Moodle matches "bob.smith@example.onmicrosoft.com"), this also adds a setting controlling minimum required username length for such matches (which defaults to 10).

--- local/o365/classes/feature/usersync/main.php
+++ local/o365/classes/feature/usersync/main.php
@@ -449,6 +449,11 @@ class main {
         $aadsync = get_config('local_o365', 'aadsync');
         $photoexpire = get_config('local_o365', 'photoexpire');
         $aadsync = array_flip(explode(',', $aadsync));
+        $switchauthminupnsplit0 = get_config('local_o365', 'switchauthminupnsplit0');
+        if (empty($switchauthminupnsplit0)) {
+            $switchauthminupnsplit0 = 10;
+        }
+
 
         $usernames = [];
         $upns = [];
@@ -574,8 +579,10 @@ class main {
                 $existinguser = null;
                 if (isset($existingusers[$user['upnlower']])) {
                     $existinguser = $existingusers[$user['upnlower']];
+                    $exactmatch = true;
                 } else if (isset($existingusers[$user['upnsplit0']])) {
                     $existinguser = $existingusers[$user['upnsplit0']];
+                    $exactmatch = strlen($user['upnsplit0']) >= $switchauthminupnsplit0;
                 }
                 // Assign user to app if not already assigned.
                 if (empty($existinguser->assigned)) {
@@ -599,29 +606,50 @@ class main {
                     }
                 }
 
-                if ($existinguser->auth !== 'oidc' && empty($existinguser->tokid)) {
+                if ($existinguser->auth !== 'oidc') {
                     $this->mtrace('Found a user in Azure AD that seems to match a user in Moodle');
                     $this->mtrace(sprintf('moodle username: %s, aad upn: %s', $existinguser->username, $user['upnlower']));
-
                     if (!isset($aadsync['match'])) {
                         $this->mtrace('Not matching user because that sync option is disabled.');
                         continue;
                     }
 
-                    if (!empty($existinguser->existingconnectionid)) {
+                    if (isset($aadsync['matchswitchauth']) && $exactmatch) {
+                        // Switch the user to OpenID authentication method, but only if this setting is enabled and full username matched
+                        require_once($CFG->dirroot.'/user/profile/lib.php');
+                        require_once($CFG->dirroot.'/user/lib.php');
+                        if (!empty($existinguser->existingconnectionid)) {
+                            // Delete existing connection before linking (in case matching was performed without auth switching previously)
+                            $DB->delete_records_select ('local_o365_connections', "id = {$existinguser->existingconnectionid}");
+                        }
+                        $fullexistinguser = get_complete_user_data('username', $existinguser->username);
+                        $existinguser->id = $fullexistinguser->id;
+                        $existinguser->auth = 'oidc';
+                        user_update_user($existinguser, true);
+                        // clear user's password
+                        $password = null;
+                        update_internal_user_password($existinguser, $password);
+                        $this->mtrace('Switched user to OpenID.');
+
+                    } else if (!empty($existinguser->existingconnectionid)) {
                         $this->mtrace('User is already matched.');
                         continue;
-                    }
 
-                    // Match to o365 account, if enabled.
-                    $matchrec = [
-                        'muserid' => $existinguser->muserid,
-                        'aadupn' => $user['upnlower'],
-                        'uselogin' => (isset($aadsync['matchswitchauth'])) ? 1 : 0,
-                    ];
-                    $DB->insert_record('local_o365_connections', $matchrec);
-                    $this->mtrace('Matched user.');
+                    } else {
+                        // Match to o365 account, if enabled
+                        $matchrec = [
+                            'muserid' => $existinguser->muserid,
+                            'aadupn' => $user['upnlower'],
+                            'uselogin' => isset($aadsync['matchswitchauth']) ? 1 : 0,
+                        ];
+                        $DB->insert_record('local_o365_connections', $matchrec);
+                        $this->mtrace('Matched user, but did not switch them to OpenID.');
+                    }
                 } else {
+                    $this->mtrace('The user is already using OpenID for authentication.');
+                }
+
+                if ($existinguser->auth === 'oidc' || empty($existinguser->tokid)) {
                     // Create userobject if it does not exist.
                     if (empty($existinguser->objrecid)) {
                         $this->mtrace('Adding o365 object record for user.');
@@ -638,7 +666,7 @@ class main {
                         $userobjectdata->id = $DB->insert_record('local_o365_objects', $userobjectdata);
                     }
                     // User already connected.
-                    $this->mtrace('User is already synced.');
+                    $this->mtrace('User is now synced.');
                 }
             }
         }
