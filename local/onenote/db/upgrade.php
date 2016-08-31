caa0cf1ac61d675cebf2650193612d22f167776d
James McQuillan <james.mcquillan@remote-learner.net>
2016-08-31 03:09:49 -0400

    MSFTMPP-573: Add check for table existence before attempting to rename

--- local/onenote/db/upgrade.php
+++ local/onenote/db/upgrade.php
@@ -99,13 +99,17 @@ function xmldb_local_onenote_upgrade($oldversion) {
     if ($oldversion < 2015011616) {
         // Define table to be renamed.
         $table = new xmldb_table('onenote_user_sections');
-        // Rename the table to use the correct Moodle naming convention.
-        $dbman->rename_table($table, 'local_onenote_user_sections');
+        if ($dbman->table_exists($table)) {
+            // Rename the table to use the correct Moodle naming convention.
+            $dbman->rename_table($table, 'local_onenote_user_sections');
+        }
 
         // Define table to be renamed.
         $table = new xmldb_table('onenote_assign_pages');
-        // Rename the table to use the correct Moodle naming convention.
-        $dbman->rename_table($table, 'local_onenote_assign_pages');
+        if ($dbman->table_exists($table)) {
+            // Rename the table to use the correct Moodle naming convention.
+            $dbman->rename_table($table, 'local_onenote_assign_pages');
+        }
 
         // Onenote savepoint reached.
         upgrade_plugin_savepoint(true, 2015011616, 'local', 'onenote');
