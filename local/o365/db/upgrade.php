6477534aa2eda011467285e0fc777e4dc170b95b
James McQuillan <james.mcquillan@remote-learner.net>
2016-11-01 02:43:32 -0400

    MSFTMPP-580: Change Graph API enable setting to a disable setting. Enable Graph API by default

--- local/o365/db/upgrade.php
+++ local/o365/db/upgrade.php
@@ -539,5 +539,15 @@ function xmldb_local_o365_upgrade($oldversion) {
         upgrade_plugin_savepoint($result, '2015060133.01', 'local', 'o365');
     }
 
+    if ($result && $oldversion < 2015060136.01) {
+        $enableunifiedapi = get_config('local_o365', 'enableunifiedapi');
+        if (empty($enableunifiedapi)) {
+            set_config('disablegraphapi', 1, 'local_o365');
+        } else {
+            set_config('disablegraphapi', 0, 'local_o365');
+        }
+        upgrade_plugin_savepoint($result, '2015060136.01', 'local', 'o365');
+    }
+
     return $result;
 }
--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015060136;
+$plugin->version = 2015060136.01;
 $plugin->requires = 2015051100;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
