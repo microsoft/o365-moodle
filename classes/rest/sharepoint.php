df16a5e5f862cb8c099f62fd915f8e06769fb29b
James McQuillan <james.mcquillan@remote-learner.net>
2016-11-11 12:39:33 -0500

    MSFTMPP-593: Add meaningful user agent string to api calls

--- classes/rest/sharepoint.php
+++ classes/rest/sharepoint.php
@@ -27,6 +27,9 @@ namespace local_o365\rest;
  * API client for SharePoint.
  */
 class sharepoint extends \local_o365\rest\o365api {
+    /** The general API area of the class. */
+    public $apiarea = 'sharepoint';
+
     /** @var string The site we're accessing. */
     protected $parentsite = '';
 
