5e381c0f31fd6302500d30ba72384969dc7a1bae
James McQuillan <james.mcquillan@remote-learner.net>
2016-11-11 12:39:33 -0500

    MSFTMPP-593: Add meaningful user agent string to api calls

--- classes/rest/discovery.php
+++ classes/rest/discovery.php
@@ -27,6 +27,9 @@ namespace local_o365\rest;
  * API client for service discovery.
  */
 class discovery extends \local_o365\rest\o365api {
+    /** The general API area of the class. */
+    public $apiarea = 'discovery';
+
     /**
      * Get the API client's oauth2 resource.
      *
