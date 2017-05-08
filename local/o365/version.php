71f325032f5338b700a8c1340bda836fb34338bc
James McQuillan <james.mcquillan@remote-learner.net>
2017-05-08 04:00:20 -0400

    MSFTMPP-627: Add workflow for adding additional tenants

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015011659;
+$plugin->version = 2015011659.06;
 $plugin->requires = 2014051200;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
