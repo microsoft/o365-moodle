85cf6e2057ee190207e0ed731f7225e54af179b2
James McQuillan <james.mcquillan@remote-learner.net>
2017-05-11 02:15:52 -0400

    MSFTMPP-630: Release v20170511_m29

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015060121;
+$plugin->version = 2015060122;
 $plugin->requires = 2015051100;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '29.0.0.20';
+$plugin->release = '29.0.0.21';
