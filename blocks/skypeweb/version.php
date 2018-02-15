0116a85514abc799e887c3b9f5379e67a49e62b2
James McQuillan <james.mcquillan@remote-learner.net>
2018-02-15 14:44:21 -0500

    MSFTMPP-650: Release v20180215_m34

--- blocks/skypeweb/version.php
+++ blocks/skypeweb/version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017111300;
+$plugin->version = 2017111301;
 $plugin->requires = 2017111300;
 $plugin->component = 'block_skypeweb';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.4.0.0';
+$plugin->release = '3.4.0.1';
--- mod/assign/submission/onenote/version.php
+++ mod/assign/submission/onenote/version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017111300;
+$plugin->version = 2017111301;
 $plugin->requires = 2017111300;
 $plugin->component = 'assignsubmission_onenote';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.4.0.0';
+$plugin->release = '3.4.0.1';
 $plugin->dependencies = [
     'local_onenote' => 2017111300,
 ];
