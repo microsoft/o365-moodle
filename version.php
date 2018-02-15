4bc8adbdd2e63194fa3f4f7c47be3ebbf985a46e
James McQuillan <james.mcquillan@remote-learner.net>
2018-02-15 14:44:21 -0500

    MSFTMPP-650: Release v20180215_m34

--- version.php
+++ version.php
@@ -23,11 +23,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017111300;
+$plugin->version = 2017111301;
 $plugin->requires = 2017111300;
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.4.0.0';
+$plugin->release = '3.4.0.1';
 $plugin->dependencies = [
     'auth_oidc' => 2017111300,
 ];
