74134dc85741659f4b68b42f4281389da7307fef
James McQuillan <james.mcquillan@remote-learner.net>
2018-02-15 14:50:12 -0500

    MSFTMPP-650: Release v20180215_m33

--- mod/assign/submission/onenote/version.php
+++ mod/assign/submission/onenote/version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017051500;
+$plugin->version = 2017051501;
 $plugin->requires = 2017051500;
 $plugin->component = 'assignsubmission_onenote';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.3.0.0';
+$plugin->release = '3.3.0.1';
 $plugin->dependencies = [
     'local_onenote' => 2017051500,
 ];
