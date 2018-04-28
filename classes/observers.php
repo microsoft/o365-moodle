92094fdb2ea5bc55119426d29e2b3edcbc35cb19
James McQuillan <james.mcquillan@remote-learner.net>
2018-04-28 18:51:31 -0400

    MSFTMPP-676: Fix detect service resource during plugin setup

--- classes/observers.php
+++ classes/observers.php
@@ -68,6 +68,19 @@ class observers {
                 break;
 
             case 'adminconsent':
+                // Get tenant if using app-only access.
+                if (\local_o365\utils::is_enabled_apponlyaccess() === true) {
+                    if (isset($eventdata['other']['tokenparams']['id_token'])) {
+                        $idtoken = $eventdata['other']['tokenparams']['id_token'];
+                        $idtoken = \auth_oidc\jwt::instance_from_encoded($idtoken);
+                        if (!empty($idtoken)) {
+                            $tenant = \local_o365\utils::get_tenant_from_idtoken($idtoken);
+                            if (!empty($tenant)) {
+                                set_config('aadtenantid', $tenant, 'local_o365');
+                            }
+                        }
+                    }
+                }
                 redirect(new \moodle_url('/admin/settings.php?section=local_o365'));
                 break;
 
