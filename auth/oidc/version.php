bb3b0df7557207a84b8c377218ec880715d8b876
James McQuillan <james.mcquillan@remote-learner.net>
2018-04-30 16:28:55 -0400

    MSFTMPP-667: Release v20180430_m33

--- auth/oidc/version.php
+++ auth/oidc/version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017051500;
+$plugin->version = 2017051501;
 $plugin->requires = 2017051500;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.3.0.0';
+$plugin->release = '3.3.0.1';
