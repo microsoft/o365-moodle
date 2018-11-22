6b79d18fcc261df53fb889c917813f96924b0b3b
Tomasz Muras <tm@enovation.ie>
2018-11-22 16:28:31 +0100

    Fixes based on the Moodle CR tool.

--- local/o365/classes/form/tabconfiguration.php
+++ local/o365/classes/form/tabconfiguration.php
@@ -1,12 +1,31 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * Created by PhpStorm.
- * User: weilai
- * Date: 01/10/2018
- * Time: 10:19
+ * @package local_o365
+ * @author  Enovation Solutions
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright (C) 2016 onwards Microsoft Open Technologies, Inc. (http://msopentech.com/)
  */
 
 namespace local_o365\form;
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->dirroot . '/lib/formslib.php');
 
 /**
  * A form for configuring of Moodle tab in Teams.
@@ -14,14 +33,6 @@ namespace local_o365\form;
  * @author Lai Wei <lai.wei@enovation.ie>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @copyright (C) 2018 onwards Microsoft, Inc. (http://microsoft.com/)
- */
-
-defined('MOODLE_INTERNAL') || die;
-
-require_once($CFG->dirroot . '/lib/formslib.php');
-
-/**
- * Tab configuration form class
  */
 class tabconfiguration extends \moodleform {
 
@@ -36,14 +47,14 @@ class tabconfiguration extends \moodleform {
 
         $courseoptions = self::get_course_options();
         if ($courseoptions) {
-            // User can access at least one course, show tab name field and course selector
+            // User can access at least one course, show tab name field and course selector.
             $mform->addElement('text', 'tab_name', get_string('tab_name', 'local_o365'),
-                array('onchange' => 'onTabNameChange()'));
+                    array('onchange' => 'onTabNameChange()'));
             $mform->setType('tab_name', PARAM_TEXT);
             $mform->setDefault('tab_name', get_string('tab_moodle', 'local_o365'));
 
-            $courseselector = $mform->createElement('select', 'course', get_string('course_selector_label','local_o365'), $courseoptions,
-                array('onchange' => 'onCourseChange()'));
+            $courseselector = $mform->createElement('select', 'course', get_string('course_selector_label', 'local_o365'),
+                    $courseoptions, array('onchange' => 'onCourseChange()'));
             $courseselector->setSize(100);
             $courseselector->setMultiple(true);
 
@@ -51,7 +62,7 @@ class tabconfiguration extends \moodleform {
             $mform->setType('course', PARAM_INT);
 
         } else {
-            // User cannot access any course, show message
+            // User cannot access any course, show message.
             $messagehtml = \html_writer::tag('p', get_string('teams_no_course', 'local_o365'));
             $mform->addElement('html', $messagehtml);
         }
