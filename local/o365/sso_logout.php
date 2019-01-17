a4089dc5a4f39d99bc42c1dd6ba0a39e6a18b0d1
Lai Wei <lai.wei@enovation.ie>
2019-01-17 16:57:28 +0000

    add log out page

---
+++ local/o365/sso_logout.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This page log an auth_oidc user out.
+ *
+ * @package local_o365
+ * @author Lai Wei <lai.wei@enovation.ie>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright (C) 2018 onwards Microsoft, Inc. (http://microsoft.com/)
+ */
+
+require_once(__DIR__ . '/../../config.php');
+
+if ($USER->id) {
+    if ($USER->auth == 'oidc') {
+        require_logout();
+    }
+}
+
+exit();
