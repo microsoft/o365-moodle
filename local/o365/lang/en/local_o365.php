b42dbd585026784d9c0d65ae377cbd9b3532dd94
James McQuillan <james.mcquillan@remote-learner.net>
2019-01-19 00:05:54 -0500

    MSFTMPP-729: Add language string for bot task

--- local/o365/lang/en/local_o365.php
+++ local/o365/lang/en/local_o365.php
@@ -470,6 +470,7 @@ $string['settings_userconnections_details'] = 'Review and manage connections bet
 $string['spsite_group_contributors_name'] = '{$a} contributors';
 $string['spsite_group_contributors_desc'] = 'All users who have access to manage files for course {$a}';
 
+$string['task_bot'] = 'Bot message task';
 $string['task_calendarsyncin'] = 'Sync o365 events in to Moodle';
 $string['task_groupcreate'] = 'Create user groups in Office 365';
 $string['task_refreshsystemrefreshtoken'] = 'Refresh system API user refresh token';
