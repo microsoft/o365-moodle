76020fda9afd81d37073961b5bfe83d04c27bd3e
James McQuillan <james.mcquillan@remote-learner.net>
2019-10-10 01:30:34 -0400

    MSFTMPP-779: Update version files (v20191007_m34)

--- auth/oidc/version.php
+++ auth/oidc/version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017111305;
+$plugin->version = 2017111306;
 $plugin->requires = 2017111300;
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.4.0.4';
+$plugin->release = '3.4.0.5';
