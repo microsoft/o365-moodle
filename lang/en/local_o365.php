d336d17bfe3f0746b6c0ec82c3126647d380b5ad
avegys <andrius.vegys@enovation.ie>
2020-04-23 17:43:38 +0300

    #1188 fix for M3.7

--- lang/en/local_o365.php
+++ lang/en/local_o365.php
@@ -288,6 +288,7 @@ $string['settings_aadsync_appassign'] = 'Assign users to application during sync
 $string['settings_aadsync_photosync'] = 'Sync Office 365 profile photos to Moodle in cron job';
 $string['settings_aadsync_photosynconlogin'] = 'Sync Office 365 profile photos to Moodle on login';
 $string['settings_aadsync_nodelta'] = 'Perform a full sync each run';
+$string['settings_aadsync_emailsync'] = 'Match Azure usernames to moodle emails instead of moodle usernames during the sync';
 $string['settings_previewfeatures'] = 'Enable preview features';
 $string['settings_previewfeatures_details'] = 'Enable features provided on a "preview" basis. These features use brand new APIs, or are experimental in some way. These features may be more prone to break, but will give you a sneak peak at what\'s coming in the near future.';
 $string['settings_photoexpire'] = 'Profile photo refresh time';
@@ -638,6 +639,8 @@ $string['help_user_photosynconlogin'] = 'Sync Office 365 Profile Photos (Login) 
 $string['help_user_photosynconlogin_help'] = 'This will cause a user\'s Moodle profile photo to get synced with their Office 365 profile photo when that user logs in.';
 $string['help_user_nodelta'] = 'Perform a full sync help';
 $string['help_user_nodelta_help'] = 'By default, user sync will only sync changes from Azure AD. Checking this option will force a full user sync each time.';
+$string['help_user_emailsync'] = 'Sync azure usernames to moodle emails Help';
+$string['help_user_emailsync_help'] = 'Enabling this option will match azure usernames to moodle emails, instead of the default behaviour which is azure usernames to moodle usernames.';
 
 $string['assignment'] = 'Assignment';
 $string['course_assignment_submitted_due'] = 'Course - {$a->course} &nbsp; |  &nbsp; Assignment -{$a->assignment} <br />
