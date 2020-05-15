8c8b2e9ed55c7c412ed9ef833b95a9bae19194ef
avegys <andrius.vegys@enovation.ie>
2020-05-15 16:01:16 +0300

    Versions updated and prettifying users sync error message

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019052004;
+$plugin->version = 2019052005;
 $plugin->requires = 2019052000;
-$plugin->release = '3.7.0.4';
+$plugin->release = '3.7.0.5';
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
