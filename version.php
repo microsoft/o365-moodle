ae82e46ace4f3ceaaaf0874086ceaa6f04550c55
avegys <andrius.vegys@enovation.ie>
2020-05-15 15:11:54 +0300

    Modified plugins versions updated

--- version.php
+++ version.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2020020302;
 $plugin->requires = 2019111800;
-$plugin->release = '3.8.0.1';
+$plugin->release = '3.8.0.2';
 $plugin->component = 'local_o365';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
