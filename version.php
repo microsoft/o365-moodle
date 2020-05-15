989814e2671f25b7e7a20ad04ab6099273478297
avegys <andrius.vegys@enovation.ie>
2020-05-15 15:11:54 +0300

    Modified plugins versions updated

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2020020301;
 $plugin->requires = 2019111800;
-$plugin->release = '3.8.0.0';
+$plugin->release = '3.8.0.1';
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
