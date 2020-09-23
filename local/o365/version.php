81fe2d8017a53c9fcf850d476e02bb782d1193d4
Lai Wei <lai.wei@enovation.ie>
2020-09-23 15:51:50 +0100

    update manifest file to include values required for SSO

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020071500;
+$plugin->version = 2020071502;
 $plugin->requires = 2020061500;
 $plugin->release = '3.9.0';
 $plugin->component = 'local_o365';
