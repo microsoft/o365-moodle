df8e7ea8b022b76d0565a5233c0136128471083f
Lai Wei <lai.wei@enovation.ie>
2020-10-09 11:18:51 +0100

    Change "facsimileTelephoneNumber" to "faxNumber" in Azure AD user attributes

--- local/o365/version.php
+++ local/o365/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020020306;
+$plugin->version = 2020020307;
 $plugin->requires = 2019111800;
 $plugin->release = '3.8.0.4';
 $plugin->component = 'local_o365';
