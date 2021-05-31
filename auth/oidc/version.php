d129f6b16ee9c69ede744230ca8e3608b4b47f52
Lai Wei <lai.wei@enovation.ie>
2021-05-31 10:49:12 +0100

    Add additional index to auth_oidc_token table for performance improvement

--- auth/oidc/version.php
+++ auth/oidc/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020071504;
+$plugin->version = 2020071505;
 $plugin->requires = 2020061500;
 $plugin->release = '3.9.4';
 $plugin->component = 'auth_oidc';
