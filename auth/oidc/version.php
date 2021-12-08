6f5bc4f8685df91b75edb5aec3b48fc98ce89ca6
Lai Wei <lai.wei@enovation.ie>
2021-12-08 15:23:54 +0000

    Update plugin version

--- auth/oidc/version.php
+++ auth/oidc/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020071540;
+$plugin->version = 2020071541;
 $plugin->requires = 2020061500;
 $plugin->release = '3.9.8';
 $plugin->component = 'auth_oidc';
