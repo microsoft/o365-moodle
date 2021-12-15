2e8706ede67131bf4773a99e3cd09e489bd39e2d
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2021-12-16 10:31:31 +1100

    Add null privacy provider for theme_boost_o365teams
    Co-authored-by: Anupama Dharmajan <anupamadharmajan@catalyst-au.net>

--- theme/boost_o365teams/version.php
+++ theme/boost_o365teams/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020071540;
+$plugin->version = 2020071541;
 $plugin->requires = 2020061500;
 $plugin->release = '3.9.8';
 $plugin->component = 'theme_boost_o365teams';
