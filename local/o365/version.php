42affea6404dfc8a61916af2977f1235e53e1703
Lai Wei <lai.wei@enovation.ie>
2022-04-07 10:14:39 +0100

    Update version for 3.9.9 supplement release

--- local/o365/version.php
+++ local/o365/version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020071550;
+$plugin->version = 2020071551;
 $plugin->requires = 2020061500;
 $plugin->release = '3.9.9';
 $plugin->component = 'local_o365';
