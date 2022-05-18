702117e34bdf2f56b55239e0690a04acb7abcbd4
Lai Wei <lai.wei@enovation.ie>
2022-05-18 12:09:22 +0100

    Allow setting schedule for suspension feature in user sync task

--- local/o365/version.php
+++ local/o365/version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021051723;
+$plugin->version = 2021051724;
 $plugin->requires = 2021051700;
 $plugin->release = '3.11.3';
 $plugin->component = 'local_o365';
