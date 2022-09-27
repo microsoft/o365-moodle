0052eff67ab498236579b945f1e1a3c368f23e89
Lai Wei <lai.wei@enovation.ie>
2022-09-27 10:54:55 +0100

    Update group mailNickname length check

--- local/o365/classes/feature/coursesync/main.php
+++ local/o365/classes/feature/coursesync/main.php
@@ -951,6 +951,9 @@ class main {
                     $resetgroupnameprefix = 'disconnected-';
                 }
                 $updatedmailnickname = $resetgroupnameprefix . utils::get_group_mail_alias($course);
+                if (strlen($updatedmailnickname) > 59) {
+                    $updatedmailnickname = substr($updatedmailnickname, 0, 59);
+                }
                 $updatedexistinggroup = [
                     'id' => $existinggroup['id'],
                     'mailNickname' => $updatedmailnickname,
