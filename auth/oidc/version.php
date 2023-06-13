18e49442691f8025d34797b5ab5e14bae9f67257
Lai Wei <lai.wei@enovation.ie>
2023-06-13 09:25:19 +0100

    Support syncing "onPremisesSamAccountName" from Azure AD

--- auth/oidc/version.php
+++ auth/oidc/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022112805;
+$plugin->version = 2022112806;
 $plugin->requires = 2022112800;
 $plugin->release = '4.1.1';
 $plugin->component = 'auth_oidc';
