be33deba156e394ee1306e139fbb0e3a3aaaff74
Lai Wei <lai.wei@enovation.ie>
2024-02-28 15:17:40 +0000

    Update auth_oidc configuration page setting availability control

--- auth/oidc/js/module.js
+++ auth/oidc/js/module.js
@@ -8,6 +8,9 @@ M.auth_oidc.init = function(Y, idptype_ms, authmethodsecret, authmethodcertifica
     var $clientsecret = $("#id_clientsecret");
     var $clientcert = $("#id_clientcert");
     var $clientprivatekey = $("#id_clientprivatekey");
+    var $clientprivatekeyfile = $("#id_clientprivatekeyfile");
+    var $clientcertfile = $("#id_clientcertfile");
+    var $clientcertpassphrase = $("#id_clientcertpassphrase");
 
     $idptype.change(function() {
         if ($(this).val() != idptype_ms) {
@@ -18,6 +21,9 @@ M.auth_oidc.init = function(Y, idptype_ms, authmethodsecret, authmethodcertifica
             $clientsecret.prop('disabled', false);
             $clientcert.prop('disabled', true);
             $clientprivatekey.prop('disabled', true);
+            $clientprivatekeyfile.prop('disabled', true);
+            $clientcertfile.prop('disabled', true);
+            $clientcertpassphrase.prop('disabled', true);
         } else {
             $clientauthmethod.append("<option value='" + authmethodcertificate + "'>" + authmethodcertificatetext + "</option>");
         }
@@ -27,6 +33,9 @@ M.auth_oidc.init = function(Y, idptype_ms, authmethodsecret, authmethodcertifica
         if ($(this).val() == authmethodcertificate) {
             $clientcert.prop('disabled', false);
             $clientprivatekey.prop('disabled', false);
+            $clientprivatekeyfile.prop('disabled', false);
+            $clientcertfile.prop('disabled', false);
+            $clientcertpassphrase.prop('disabled', false);
         }
     });
 };
