c516183a15903d918dd6fd16ecac1b12e7fb5b81
Lai Wei <lai.wei@enovation.ie>
2024-07-01 12:39:10 +0100

    bug fix in creating connection records for new accounts in user sync task run

--- local/o365/classes/feature/usersync/main.php
+++ local/o365/classes/feature/usersync/main.php
@@ -910,7 +910,7 @@ class main {
                 'type' => 'user',
                 'subtype' => '',
                 'objectid' => $userobjectid,
-                'o365name' => $entraiduserdata['userPrincipalName'],
+                'o365name' => $entraiduserdata['useridentifier'],
                 'moodleid' => $newuser->id,
                 'tenant' => '',
                 'timecreated' => $now,
