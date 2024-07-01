41b894bbc2a69e319edb52a1b1285846a35b4755
Lai Wei <lai.wei@enovation.ie>
2024-07-01 17:11:31 +0100

    Update plugin versions for 4.3.4 release

--- blocks/microsoft/version.php
+++ blocks/microsoft/version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023100910;
+$plugin->version = 2023100920;
 $plugin->requires = 2023100900;
-$plugin->release = '4.3.1';
+$plugin->release = '4.3.4';
 $plugin->component = 'block_microsoft';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'local_o365' => 2023100910,
+    'local_o365' => 2023100920,
 ];
--- local/office365/version.php
+++ local/office365/version.php
@@ -25,15 +25,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023100915;
+$plugin->version = 2023100920;
 $plugin->requires = 2023100900;
-$plugin->release = '4.3.3';
+$plugin->release = '4.3.4';
 $plugin->component = 'local_office365';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'auth_oidc' => 2023100915,
-    'block_microsoft' => 2023100910,
-    'local_o365' => 2023100911,
+    'auth_oidc' => 2023100920,
+    'block_microsoft' => 2023100920,
+    'local_o365' => 2023100920,
     'repository_office365' => 2023100910,
     'theme_boost_o365teams' => 2023100900,
 ];
