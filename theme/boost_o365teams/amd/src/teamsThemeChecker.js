8860e5dbcb872d89ff56677c89d9d6db41f0663b
Lai Wei <lai.wei@enovation.ie>
2024-10-24 16:41:33 +0100

    Fix Moodle plugin CI errors in theme_boost_o365teams

--- theme/boost_o365teams/amd/src/teamsThemeChecker.js
+++ theme/boost_o365teams/amd/src/teamsThemeChecker.js
@@ -1,19 +1,17 @@
-define(['jquery'], function($) {
+define(['jquery', 'microsoftTeams'], function($, microsoftTeams) {
     return {
         init: function() {
-            if (typeof microsoftTeams !== 'undefined') {
-                microsoftTeams.initialize();
+            microsoftTeams.initialize();
 
-                microsoftTeams.getContext(function(context) {
-                    var theme = context.theme;
-                    $("body").addClass("theme_" + theme);
-                });
+            microsoftTeams.getContext(function(context) {
+                var theme = context.theme;
+                $("body").addClass("theme_" + theme);
+            });
 
-                microsoftTeams.registerOnThemeChangeHandler(function(theme) {
-                    $("body").removeClass("theme_default theme_dark theme_contrast");
-                    $("body").addClass("theme_" + theme);
-                });
-            }
+            microsoftTeams.registerOnThemeChangeHandler(function(theme) {
+                $("body").removeClass("theme_default theme_dark theme_contrast");
+                $("body").addClass("theme_" + theme);
+            });
         }
     };
 });
