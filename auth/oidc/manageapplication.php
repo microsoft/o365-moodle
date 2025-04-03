094e8b81f47c645c671dd538655345b97693a0ff
Thomas Legay <thomas@lgy.fr>
2025-04-03 15:07:20 +0200

    Allow use custom claims from token

--- auth/oidc/manageapplication.php
+++ auth/oidc/manageapplication.php
@@ -60,7 +60,7 @@ foreach (
         'idptype', 'clientid', 'clientauthmethod', 'clientsecret', 'clientprivatekey', 'clientcert',
         'clientcertsource', 'clientprivatekeyfile', 'clientcertfile', 'clientcertpassphrase',
         'authendpoint', 'tokenendpoint', 'oidcresource', 'oidcscope', 'secretexpiryrecipients',
-        'bindingusernameclaim', 'customclaimname',
+        'bindingusernameclaim', 'customclaimname', 'customclaims',
     ] as $field
 ) {
     if (isset($oidcconfig->$field)) {
@@ -80,7 +80,7 @@ if ($form->is_cancelled()) {
 
     // Prepare config settings to save.
     $configstosave = ['idptype', 'clientid', 'clientauthmethod', 'authendpoint', 'tokenendpoint',
-        'oidcresource', 'oidcscope'];
+        'oidcresource', 'oidcscope','customclaims'];
 
     // Depending on the value of clientauthmethod, save clientsecret or (clientprivatekey and clientcert).
     switch ($fromform->clientauthmethod) {
