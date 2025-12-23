d9643c834f5a6dd6360cc43da2a0c5721c27fd19
Patryk Mroczko <patryk.mroczko@enovation.ie>
2025-12-23 12:05:07 +0100

    Update plugin versions for 4.1.14 release

--- local/office365/version.php
+++ local/office365/version.php
@@ -25,15 +25,15 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022112860;
+$plugin->version = 2022112865;
 $plugin->requires = 2022112800;
-$plugin->release = '4.1.13';
+$plugin->release = '4.1.14';
 $plugin->component = 'local_office365';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
     'auth_oidc' => 2022112855,
     'block_microsoft' => 2022112855,
-    'local_o365' => 2022112860,
+    'local_o365' => 2022112865,
     'repository_office365' => 2022112855,
     'theme_boost_o365teams' => 2022112855,
 ];
