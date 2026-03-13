2d701809f4458a388a2c49b5c921d6be16da8b06
Ivan <theqoocjil@MacBook-Pro-Ivan.local>
2026-03-13 17:49:21 +0300

    fixed a bug with a non-existent key in the project for custom moodle and idp connection

--- auth/oidc/classes/loginflow/base.php
+++ auth/oidc/classes/loginflow/base.php
@@ -752,7 +752,7 @@ class base {
 
         switch ($bindingusernameclaim) {
             case 'custom':
-                $bindingusernameclaim = get_config('auth_oidc', 'custombindingclaim');
+                $bindingusernameclaim = get_config('auth_oidc', 'customclaimname');
                 // No break.
             case 'preferred_username':
             case 'email':
