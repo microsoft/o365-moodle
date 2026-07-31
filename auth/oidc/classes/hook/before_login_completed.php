f3b0726a459e77c840ed851453bed675c20e4f12
Lai Wei <lai.wei@enovation.ie>
2026-07-31 16:35:03 +0100

    auth_oidc: address review feedback for before_login_completed hook
    - Dispatch the hook in all three handlelogin() branches (it previously
      only fired for already-connected users, missing the username-changed
      and new/unconnected-user paths - the latter being the main case for
      gating first-time logins).
    - Dispatch only after authenticate_user_login() succeeds, immediately
      before complete_user_login(), instead of unconditionally.
    - Fix the hook's PHPDoc/attribute text and $idtoken property doc, which
      were copy-pasted from unrelated core hooks.
    - Add the missing @param tag for the constructor.
    - Document the login-rejection contract: callbacks reject a login by
      throwing an exception (e.g. \moodle_exception), since the hook manager
      does not catch callback exceptions.

--- auth/oidc/classes/hook/before_login_completed.php
+++ auth/oidc/classes/hook/before_login_completed.php
@@ -19,20 +19,31 @@ namespace auth_oidc\hook;
 use auth_oidc\jwt;
 
 /**
- * Allow plugins to callback as soon possible after user has completed login.
+ * Allow plugins to perform additional checks before a user login is completed.
+ *
+ * This hook is dispatched by auth_oidc after authenticate_user_login() has
+ * succeeded, but before complete_user_login() is called. The hook manager
+ * does not catch exceptions raised by callbacks, so a callback can reject
+ * the login by throwing an exception (e.g. \moodle_exception) - doing so
+ * will propagate out of the hook dispatch and prevent complete_user_login()
+ * from being called. There is no other signal (e.g. a flag on this hook) to
+ * reject the login; a plain return from a callback allows the login to
+ * proceed.
  *
  * @package    auth_oidc
  * @copyright  2026 Ariadne
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-#[\core\attribute\label('Allow plugins to callback as soon possible after user has completed login.')]
+#[\core\attribute\label('Allow plugins to perform additional checks before a user login is completed.')]
 #[\core\attribute\tags('user', 'login')]
 class before_login_completed {
     /**
      * Constructor for the hook.
+     *
+     * @param jwt $idtoken The id_token of the user attempting to log in.
      */
     public function __construct(
-        /** @var jwt The course instance */
+        /** @var jwt The id_token of the user attempting to log in */
         public readonly jwt $idtoken
     ) {
     }
