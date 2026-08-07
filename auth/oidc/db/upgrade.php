65706f61a320231a50b16e455a43cee8c70ba686
Lai Wei <lai.wei@enovation.ie>
2026-08-07 18:12:28 +0100

    Update icon settings in auth_oidc

--- auth/oidc/db/upgrade.php
+++ auth/oidc/db/upgrade.php
@@ -586,6 +586,11 @@ function xmldb_auth_oidc_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2025100601.01, 'auth', 'oidc');
     }
 
+    if ($oldversion < 2025100602.05) {
+        \auth_oidc\utils::migrate_removed_icon_choices();
+        upgrade_plugin_savepoint(true, 2025100602.05, 'auth', 'oidc');
+    }
+
     return true;
 }
 
