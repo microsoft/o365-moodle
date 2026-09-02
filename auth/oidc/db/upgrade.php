a0aae5fca4536120feddf31f37f5a6ba430e84cb
Lai Wei <lai.wei@enovation.ie>
2026-09-02 09:38:23 +0100

    Use single Microsoft 365 logo for auth_oidc login icon
    Remove the separate microsoft_365 and microsoft_365_copilot icon images.
    The current microsoft.svg logo is now used for both Microsoft and
    Microsoft 365, renamed to microsoft_365.svg and referred to as the
    Microsoft 365 logo.
    
    Sites that had selected the Microsoft or Microsoft 365 Copilot icon are
    migrated to the Microsoft 365 logo on upgrade.

--- auth/oidc/db/upgrade.php
+++ auth/oidc/db/upgrade.php
@@ -591,6 +591,11 @@ function xmldb_auth_oidc_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2026042000.05, 'auth', 'oidc');
     }
 
+    if ($oldversion < 2026042000.08) {
+        \auth_oidc\utils::migrate_removed_icon_choices();
+        upgrade_plugin_savepoint(true, 2026042000.08, 'auth', 'oidc');
+    }
+
     return true;
 }
 
